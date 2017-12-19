2a1e2661cbf545bf86d0f1f1764ca6f8d8da9e16
Julia Nara <julianara888@gmail.com>
2017-12-19 01:17:30 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,20 @@ class App < Sinatra::Base
       erb :team
     end
     
+    
+    
+          @team = Team.new(name: params[:team][:name], motto: params[:team][:motto])
+      members = params[:team][:members]
+      @super_heroes = members.collect do |member_params|
+        SuperHero.new({name: member_params[:name], power: member_params[:power], bio: member_params[:bio]})
+      end
+      erb :team
+    end
     post '/teams' do
+      @team = Team.new(name: params[:team][:name], motto: params[:team][:motto])
+      members = params[:team][:members]
+      @heroes = members.collect do |member_params|
+        Hero.new({name: member_params[:name], power: member_params[:power], bio: member_params[:bio]})
       erb :super_hero
     end
 
